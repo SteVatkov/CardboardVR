9da681138927a5deaa8a81c7faf9c4d53a582a87
Steve <stevatkov@gmail.com>
2016-07-15 00:32:05 -0400

    Added Project

---
+++ CardboardVRExcercise/Assets/Scipts/BallInteraction.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallInteraction : MonoBehaviour {
+
+    public GameObject target;
+    public GameObject ballParent;
+    
+    bool ballChild = true;
+
+    // Use this for initialization
+    void Start()
+    {
+        highlighted(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(ballChild == false)
+        {
+            float smooth = 2F;
+            Vector3 targetPosition = target.transform.position;
+            transform.position = Vector3.Lerp(transform.position, targetPosition, smooth * Time.deltaTime);
+            GetComponent<Rigidbody>().velocity = Vector3.zero;
+            GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+        }
+    }
+
+    public void highlighted(bool hightlight)
+    {
+        if (hightlight == true) GetComponent<Renderer>().material.color = new Color32(255, 245, 205, 255);
+        else GetComponent<Renderer>().material.color = new Color32(255, 224, 122, 255);
+    }
+
+    public void highlightEnter()
+    {
+        highlighted(true);
+    }
+
+    public void highlightExit()
+    {
+        highlighted(false);
+    }
+
+    public void highlightClick()
+    {
+        if (ballChild == true)
+        {
+            this.gameObject.transform.parent = target.transform;
+            GetComponent<Rigidbody>().velocity = Vector3.zero;
+            GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+            GetComponent<Rigidbody>().useGravity = false;
+            ballChild = false;
+        }
+        else
+        {
+            this.gameObject.transform.parent = ballParent.transform;
+            GetComponent<Rigidbody>().useGravity = true;
+            ballChild = true;
+        }
+    }
+}
---
+++ CardboardVRExcercise/Assets/Scipts/ButtonInteract.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonInteract : MonoBehaviour {
+
+    public GameObject roof;
+    bool open = false;
+
+    Animator animator;
+
+    // Use this for initialization
+    void Start() {
+        highlighted(false);
+        animator = roof.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void highlighted(bool hightlight)
+    {
+        if (hightlight == true) GetComponent<Renderer>().material.color = Color.white;
+        else GetComponent<Renderer>().material.color = new Color32(253,89,89,255);
+    }
+
+    public void highlightEnter()
+    {
+        highlighted(true);
+    }
+
+    public void highlightExit()
+    {
+        highlighted(false);
+    }
+
+    public void highlightClick()
+    {
+        if (open == true)
+        {
+            animator.SetBool("Open", false);
+            open = false;
+        }
+        else
+        {
+            animator.SetBool("Open", true);
+            open = true;
+        }
+    }
+}
